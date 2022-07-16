bc1562bf0f4914b91e7fc002113d3aa6c5e82fdd
JoJoJet <joe102000@gmail.com>
2022-07-16 17:40:09 -0400

    merge code into one file

--- src/main.rs
+++ src/main.rs
@@ -1,6 +1,42 @@
-use bevy::prelude::*;
+use bevy::{ecs::system::SystemParam, prelude::*};
 
-mod inner;
+mod inner {
+    use super::*;
+
+    pub struct InnerPlugin;
+    impl Plugin for InnerPlugin {
+        fn build(&self, app: &mut App) {
+            app.add_startup_system(setup);
+        }
+    }
+
+    // if you mark this as `pub`, the example compiles and runs fine, but exposes an implementation detail.
+    #[derive(Component)]
+    struct Private(String);
+
+    fn setup(mut commands: Commands) {
+        commands
+            .spawn()
+            .insert(Private("this is private".to_string()));
+        commands
+            .spawn()
+            .insert(Private("this is also private".to_string()));
+    }
+
+    use bevy::ecs::system::lifetimeless::Read;
+    #[derive(SystemParam)]
+    pub struct OpaqueParams<'w, 's> {
+        q: Query<'w, 's, Read<Private>>,
+        // ...other params, etc...
+    }
+
+    // prints the value of private components
+    pub fn print_private(OpaqueParams { q }: OpaqueParams) {
+        for p in q.iter() {
+            println!("{}", p.0);
+        }
+    }
+}
 
 fn main() {
     App::new()
